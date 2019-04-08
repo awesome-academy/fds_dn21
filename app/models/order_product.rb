2e0827370599c3a6308bb8871ea8638469aa23e0
Phong Ky <nguyenvankyphong@gmail.com>
2019-04-08 14:28:16 +0700

    add-remove-from-cart

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,8 @@
 class OrderProduct < ApplicationRecord
   belongs_to :product
   belongs_to :order
+
+  validates :product_id, presence: true
+  validates :price, presence: true
+  validate :quantity_is_available
 end
